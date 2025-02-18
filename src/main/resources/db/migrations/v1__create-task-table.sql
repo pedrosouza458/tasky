29eb2f009455de1787670048a61efef4d8d7deaf
Pedro Souza <pedro.ads510@gmail.com>
2025-02-18 02:38:06 -0300

    refactor: rename task table to tasks

--- src/main/resources/db/migrations/v1__create-task-table.sql
+++ src/main/resources/db/migrations/v1__create-task-table.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-CREATE TABLE task (
+CREATE TABLE tasks (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     title VARCHAR(100) NOT NULL,
     description VARCHAR(250) NOT NULL,
