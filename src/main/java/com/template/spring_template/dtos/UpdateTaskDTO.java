a27aa1e16c08f97b5517446a166d457ff97bca0f
Pedro Souza <pedro.ads510@gmail.com>
2025-02-19 23:59:21 -0300

    feat: add dtos folder

---
+++ src/main/java/com/template/spring_template/dtos/UpdateTaskDTO.java
@@ -0,0 +1,19 @@
+package com.template.spring_template.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateTaskDTO {
+    private String title;
+    private String description;
+    private Date limitDate;
+    private Boolean done;
+}
