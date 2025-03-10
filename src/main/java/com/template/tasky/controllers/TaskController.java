60e734c9ef9e59c095dc0e2902d09cc1adc51b0d
Pedro Souza <pedro.ads510@gmail.com>
2025-03-09 21:28:09 -0300

    feat: improve task routes return

--- src/main/java/com/template/tasky/controllers/TaskController.java
+++ src/main/java/com/template/tasky/controllers/TaskController.java
@@ -1,16 +1,17 @@
 package com.template.tasky.controllers;
 
 import com.template.tasky.models.Task;
+import com.template.tasky.dtos.UpdateTaskDTO;
 import com.template.tasky.repositories.TaskRepository;
+import org.apache.coyote.Response;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 @RestController
 @RequestMapping("/tasks")
@@ -25,31 +26,50 @@ public class TaskController {
     public List<Task> getTasks(@RequestParam(defaultValue = "1") int page){
         Pageable pageable = PageRequest.of(page - 1, 20);
         Page<Task> taskPage = taskRepository.findAll(pageable);
-        return taskPage.getContent();
+        return ResponseEntity.status(HttpStatus.OK).body(taskPage.getContent()).getBody();
     }
 
     @GetMapping("/{id}")
-    public Optional<Task> getTaskById(@PathVariable UUID id){
-        return taskRepository.findById(id);
+    public ResponseEntity<Object> getTaskById(@PathVariable UUID id){
+        Optional<Task> result = taskRepository.findById(id);
+        if(result.isPresent()){
+            return ResponseEntity.status(HttpStatus.OK).body(result.get());
+        }
+        else {
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "Task not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
     }
 
     @PostMapping
-    public Task createTask(@RequestBody Task task){
-        return taskRepository.save(task);
+    public ResponseEntity<Object> createTask(@RequestBody Task task){
+        try {
+            taskRepository.save(task);
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "Task created successfully");
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTask(@PathVariable UUID id) {
+    public ResponseEntity<Object> deleteTask(@PathVariable UUID id) {
         if (taskRepository.existsById(id)) {
             taskRepository.deleteById(id);
-            return ResponseEntity.noContent().build(); // 204 No Content
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "Task created successfully");
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
         } else {
-            return ResponseEntity.notFound().build(); // 404 Not Found
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "Task not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         }
     }
 
 //    @PatchMapping("/{id}")
-//    public Task updateTask(@PathVariable UUID id, @RequestBody Task task){
+//    public Task updateTask(@PathVariable UUID id, @RequestBody Task UpdateTaskDTO){
 //
 //    }
 }
