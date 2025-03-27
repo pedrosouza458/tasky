9109eee72041240250f4b59e9a6c1782af59220e
Pedro Souza <pedro.ads510@gmail.com>
2025-03-26 23:28:21 -0300

    feat: create user class

---
+++ src/main/java/com/template/tasky/models/User.java
@@ -0,0 +1,29 @@
+package com.template.tasky.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+    private String email;
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Task> tasks;
+}
