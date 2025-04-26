a1a78856e71ebf34b8ed1dd7588bb8de1332c63f
Pedro Souza <pedro.ads510@gmail.com>
2025-04-26 19:03:11 -0300

    style: add mx and my in task item

--- web/src/components/task.tsx
+++ web/src/components/task.tsx
@@ -5,16 +5,18 @@ import { updateTaskStatus } from "../requests/update-task-status";
 export function Task({ id, title, description, done }: TaskProps) {
   return (
     <div className="flex flex-col border gap-3">
-      <div className="flex justify-between">
-        <h1>{title}</h1>
-        <button
-          onClick={() => updateTaskStatus(id, done)}
-          className="cursor-pointer"
-        >
-          <p>{done === true ? <Check /> : <X />}</p>
-        </button>
+      <div className="my-2 mx-1">
+        <div className="flex justify-between">
+          <h1>{title}</h1>
+          <button
+            onClick={() => updateTaskStatus(id, done)}
+            className="cursor-pointer"
+          >
+            <p>{done === true ? <Check /> : <X />}</p>
+          </button>
+        </div>
+        <h2>{description}</h2>
       </div>
-      <h2>{description}</h2>
     </div>
   );
 }
