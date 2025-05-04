d087ff7171bd11379d7f197e78a0545a049bc316
Pedro Souza <pedro.ads510@gmail.com>
2025-05-03 23:36:06 -0300

    feat: add user sql script

---
+++ server/src/main/resources/db/migrations/create-user.table.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
