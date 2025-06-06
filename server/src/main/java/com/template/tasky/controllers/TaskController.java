982a45c5e812c87c20a49d16f11b744f3b2489c3
Pedro Souza <pedro.ads510@gmail.com>
2025-06-05 23:21:13 -0300

    refactor: split imports

--- server/src/main/java/com/template/tasky/controllers/TaskController.java
+++ server/src/main/java/com/template/tasky/controllers/TaskController.java
@@ -10,7 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/tasks")
