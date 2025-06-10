8466ddb25434384fc254592d7c0f13dfe8e0e8dd
Pedro Souza <pedro.ads510@gmail.com>
2025-06-09 23:07:51 -0300

    refactor: change update task dto to record

--- server/src/main/java/com/template/tasky/dtos/UpdateTaskDTO.java
+++ server/src/main/java/com/template/tasky/dtos/UpdateTaskDTO.java
@@ -1,19 +1,5 @@
 package com.template.tasky.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.util.Date;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class UpdateTaskDTO {
-    private String title;
-    private String description;
-    private Date limitDate;
-    private Boolean done;
-}
+public record UpdateTaskDTO( String title, String description, Date limitDate, Boolean done){}
