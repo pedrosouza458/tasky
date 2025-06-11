7de340c40d48fcc695d826fdef81d9f41f072121
Pedro Souza <pedro.ads510@gmail.com>
2025-06-10 23:32:53 -0300

    feat: add get users function

--- server/src/main/java/com/template/tasky/controllers/UserController.java
+++ server/src/main/java/com/template/tasky/controllers/UserController.java
@@ -1,8 +1,8 @@
 package com.template.tasky.controllers;
 
-import com.template.tasky.models.Task;
-import com.template.tasky.repositories.TaskRepository;
+import com.template.tasky.models.User;
 import com.template.tasky.repositories.UserRepository;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,27 +11,40 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final UserRepository userRepository;
+   private final UserRepository userRepository;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+   public UserController(UserRepository userRepository) {
+       this.userRepository = userRepository;
+   }
 
-   @GetMapping("/{id}")
-   public ResponseEntity<Object> getUserById(@PathVariable UUID id){
-       Optional<User> result = userRepository.findById(id);
-       // Optional<User> result = userRepository.findById(id);
-       if(result.isPresent()){
-           return ResponseEntity.status(HttpStatus.OK).body(result.get());
+   @GetMapping()
+   public ResponseEntity<Object> getUsers(){
+       List<User> result = userRepository.findAll();
+
+       if(result.isEmpty()){
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
        }
        else {
-           Map<String, String> response = new HashMap<>();
-           response.put("message", "User not found");
-           return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+           return ResponseEntity.status(HttpStatus.OK).body(result);
        }
    }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Object> getUserById(@PathVariable UUID id){
+      Optional<User> result = userRepository.findById(String.valueOf(id));
+
+      if(result.isPresent()){
+          return ResponseEntity.status(HttpStatus.OK).body(result.get());
+      }
+      else {
+          Map<String, String> response = new HashMap<>();
+          response.put("message", "User not found");
+          return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+      }
+  }
 }
