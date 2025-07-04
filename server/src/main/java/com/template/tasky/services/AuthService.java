8a41ea363fc1e4889756a6670a8bc39f06667311
Pedro Souza <pedro.ads510@gmail.com>
2025-07-03 23:41:03 -0300

    feat: add get logged user function

--- server/src/main/java/com/template/tasky/services/AuthService.java
+++ server/src/main/java/com/template/tasky/services/AuthService.java
@@ -2,6 +2,9 @@ package com.template.tasky.services;
 
 import com.template.tasky.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,5 +19,18 @@ public class AuthService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return repository.findByEmail(username);
+    }
+
+    public User getLoggedUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication == null || !authentication.isAuthenticated()){
+            return null;
+        }
+        Object principal = authentication.getPrincipal();
+        if(principal instanceof User){
+            return (User) principal;
+        }
+        return null;
     }
 }
